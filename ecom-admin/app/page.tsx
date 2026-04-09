3153407f2d89b0ce4447e5a2debc838914b46353
Pratikshasaha <prathikshasaha@rishfotech.com>
2026-04-09 09:17:15 +0530

    test changes

--- ecom-admin/app/page.tsx
+++ ecom-admin/app/page.tsx
@@ -2,24 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-10 font-sans">
       <main>
-        <span className="inline-grid grid-cols-3 gap-4">
-          <span>01</span>
-          <span>02</span>
-          <span>03</span>
-          <span>04</span>
-          <span>05</span>
-          <span>06</span>
-        </span>
-        <span className="inline-grid grid-cols-3 gap-4">
-          <span>01</span>
-          <span>02</span>
-          <span>03</span>
-          <span>04</span>
-          <span>05</span>
-          <span>06</span>
-        </span>
+          <h1>Admin Panel</h1>
       </main>
     </div>
   );
